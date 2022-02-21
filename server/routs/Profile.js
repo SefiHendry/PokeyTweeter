16b982a001f52bca91c152bb7f318fc2c2208096
Sefi Hendry <sefi.h@protonmail.com>
2022-02-21 15:33:04 +0200

    implementing profile backend logic for drawing tweets

---
+++ server/routs/Profile.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+
+router.post("/profile", async (req, res) => {
+  try {
+    const { userID } = req.body;
+    pool
+      .getConnection()
+      .then((conn) => {
+        conn
+          .query(
+            "SELECT userName AS author, tweet AS body, DATE_FORMAT(createdAt, '%d/%m/%y %H:%i') as time, embedType, embedID FROM tweets JOIN users ON (tweets.userID = users.id) WHERE tweets.userID=?",
+            [userID]
+          )
+          .then((result) => {
+            conn.release();
+            return res.status(200).send(result);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).send("database error");
+            conn.release(); // release to pool
+          });
+      })
+      .catch((err) => {
+        //not connected
+        console.log(err);
+        res.status(500).send("unable to connect to the database");
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .send("internal server error while trying make a registration");
+  }
+});
+module.exports = router;
