bad5053e7d7bb873685eb2e144edd4abe8ba5e96
Sefi Hendry <sefi.h@protonmail.com>
2022-02-21 15:37:30 +0200

    added profile and tweets routs

--- app/src/App.js
+++ app/src/App.js
@@ -4,16 +4,29 @@ import HomePage from "./pages/HomePage";
 import FormRegister from "./components/register/FormRegister";
 import FormLogin from "./components/login/FormLogin";
 import Header from "./components/Header/Header";
+import TweetBox from "./components/tweetBox/TweetBox";
+import Profile from "./components/profile/Profile";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      height="100%"
+      className="App"
+      style={{
+        backgroundColor: "#191919",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
       <BrowserRouter>
+        {/* <SideBar /> */}
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/register" component={FormRegister} />
           <Route exact path="/login" component={FormLogin} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/tweetBox" component={TweetBox} />
         </Switch>
       </BrowserRouter>
     </div>
