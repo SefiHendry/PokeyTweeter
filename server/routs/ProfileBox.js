41443cf5387f3c1513c0a1d806c95cb58a620110
Sefi Hendry <sefi.h@protonmail.com>
2022-02-21 15:31:21 +0200

    Creating an infrastructure for editing profiles

---
+++ server/routs/ProfileBox.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+
+router.post("/profileBox", async (req, res) => {
+  try {
+    const { userName, userPassword } = req.body;
+    pool
+      .getConnection()
+      .then((conn) => {
+        conn
+          .query("SELECT * FROM users WHERE userName=? AND userPassword=?", [
+            userName,
+            userPassword,
+          ])
+          .then((result) => {
+            if (!result[0]) {
+              conn.release(); // release to pool
+              return res.status(400).send("user not found");
+            }
+            conn.release(); // release to pool
+            return res.status(200).send(result[0]);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).send("database error");
+            conn.release(); // release to pool
+          });
+      })
+      .catch((err) => {
+        //not connected
+        console.log(err);
+        res.status(500).send("unable to connect to the database");
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .send("internal server error while trying make a registration");
+  }
+});
+module.exports = router;
